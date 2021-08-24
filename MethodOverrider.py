f72195a22b0c105afa67ca453f67ace78eabde29
fernandocampanha <86123575+fernandocampanha@users.noreply.github.com>
2021-08-24 13:10:09 -0300

    Add files via upload

--- MethodOverrider.py
+++ MethodOverrider.py
@@ -2,7 +2,7 @@ from burp import IBurpExtender
 from burp import IHttpRequestResponse
 from burp import IContextMenuFactory
 from burp import IContextMenuInvocation
-from javax.swing import JMenuItem
+from javax.swing import (JPanel, JSplitPane, JList, JTextPane, JScrollPane, ListSelectionModel, JLabel, JTabbedPane, JEditorPane)
 from burp import IHttpService
 from threading import Thread
 from burp import IHttpListener
@@ -10,9 +10,10 @@ from burp import ITab
 from javax import swing
 from java.awt import BorderLayout
 from burp import IParameter
-from burp import IResponseVariations
+from javax.swing.text import DefaultStyledDocument
+from java.util import ArrayList
 
-class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListener, ITab, IParameter, IResponseVariations):
+class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListener, ITab, IParameter):
 
 	def registerExtenderCallbacks(self, callbacks):
 
@@ -27,11 +28,14 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 		self.parameters = ['_method', 'method', 'X-Http-Method-Override', 'X-HTTP-Method', 'X-Method-Override']
 
 		self.status = [200, 201, 301, 302, 307, 308, 400, 401, 403, 404, 405, 501]
-		# self.status = [200, 201, 301, 302, 307, 308, 501]
 
 		self.targets = []
 
-		self.results = []
+		self.requests = []
+
+		self.check = False
+
+		self.override_type = -1
 
 		callbacks.setExtensionName("MethodOverrider")
 		
@@ -40,10 +44,6 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 		callbacks.registerContextMenuFactory(self.createMenuItems)
 
 		callbacks.registerHttpListener(self)
-
-		# self.createTab()
-
-		# callbacks.addSuiteTab(self)
 
 		return 
 
@@ -70,33 +70,36 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 					thread.join()
 
 
-	def analyze_response(self, result, method):
-		responses = []
-		for i in self.results:
-			# responses.append()
-			print(i)
-		# print(responses)
-		# new_response = self.helpers.analyzeResponse(result.getResponse())
-		# status_code = new_response.getStatusCode()
+	def analyze_response(self, result, name, method):
 
-		# # Verify if the status code has changed
-		# if(status_code != self.initial_response.getStatusCode()):
+		new_response = self.helpers.analyzeResponse(result.getResponse())
+		status_code = new_response.getStatusCode()
 
-		# 	if(status_code in self.status):
-		# 		try:
-		# 			# print("novo: " + str(status_code) + "original: " + str(self.initial_response.getStatusCode()))
-		# 			self.results.append(str(status_code) + " for using " + method + " method")				
-		# 		except:
-		# 			print("Foi nao")
-		# #		print(str(status_code) + " for using " + method + " method")
+		# Verify if the status code has changed
+		if(status_code != self.initial_response.getStatusCode()):
+			
+			self.check  = True
 
+			if(status_code in self.status):
+				try:
+					path = str(self.helpers.analyzeRequest(result.getRequest()).getHeaders()[0].split()[1])
+					if(self.override_type == 0):
+						print("The page '" + result.getHttpService().getProtocol() + "://" + result.getHttpService().getHost() + 
+							":" + str(result.getHttpService().getPort()) + path + "' might have the method override technique enabled by using the header '" + name + ": " + method + "'")
+					elif(self.override_type == 1):
+						print("The page " + result.getHttpService().getProtocol() + "://" + result.getHttpService().getHost() + 
+							":" + str(result.getHttpService().getPort()) + path + " might have the method override technique enabled by using the parameter '" + name + ": " + method + "'")
+				except:
+					print("Foi nao")
 
-		# Verify if the size of response has changed
+			if(len(result.getResponse()) != len(self.initial_result.getResponse())):
+				print("The response body has different length \n")
 
-		#	if(len(result.getResponse()) != len(self.initial_result.getResponse())):
-		#		print("The response body has different length \n")
+			self._callbacks.issueAlert("Possible Http Method Override detected")
+
 
 	def sendWithHeaders(self, i, body):
+		self.override_type = 0
 
 		for header in self.headers:
 
@@ -108,9 +111,9 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 				new_request = self.helpers.buildHttpMessage(self.initial_headers, None)
 
 				try:
+
 					result = self._callbacks.makeHttpRequest(i.getHttpService(), new_request)
-					self.results.append(result.getResponse())
-					# self.analyze_response(result, method)
+					self.analyze_response(result, header, method)
 
 				except:
 
@@ -121,6 +124,7 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 				self.initial_headers.pop()
 
 	def sendWithParameter(self, i, body):
+		self.override_type = 1
 		initial_request = self.helpers.analyzeRequest(i.getRequest())
 		params = initial_request.getParameters()
 
@@ -131,9 +135,7 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 
 			 	try:
 					result = self._callbacks.makeHttpRequest(i.getHttpService(), new_request)
-					# self.results.append(result.getResponse())
-					print(self.helpers.bytesToString(result.getResponse()))
-					# self.analyze_response(result, method)
+					self.analyze_response(result, param, method)
 
 				except:
 
@@ -176,7 +178,6 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 			# Descobrir pq tem dois espacos a mais nos headers
 			newRequest.pop()
 			newRequest.pop()
-			# print(newRequest)
 
 		self.initial_headers = newRequest
 
@@ -190,14 +191,10 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 			print("foi nao")
 
 		self.sendWithHeaders(i, body)
+
 		# Just call the sendWithParameter if the header results are empty
-		if(len(self.results) == 0):
-			self.results = []
+		if(self.check == False):
 			self.sendWithParameter(i, body)	
-
-		print("The results are:\n" + str(self.results))
-		self.results = []
-
 
 	def getRequest(self, event):
 		
@@ -222,58 +219,3 @@ class BurpExtender(IBurpExtender, IHttpRequestResponse, IHttpService, IHttpListe
 		menuList.add(menuItem)
 		
 		return menuList			
-
-	def getTabCaption(self):
-		return "Method Overrider"
-    
-	def getUiComponent(self):
-		return self.tab
-
-	def encode(self, event):
-		pass
-
-	def createTab(self):
-
-		self.tab = swing.JPanel(BorderLayout())
-
-		textPanel = swing.JPanel()
-		
-		boxVertical = swing.Box.createVerticalBox()
-		boxHorizontal = swing.Box.createHorizontalBox()
-		textLabel = swing.JLabel("Text to be encoded/decoded/hashed")
-		boxHorizontal.add(textLabel)
-		boxVertical.add(boxHorizontal)
-
-		boxHorizontal = swing.Box.createHorizontalBox()
-		self.textArea = swing.JTextArea('', 6, 100)
-		self.textArea.setLineWrap(True)
-		boxHorizontal.add(self.textArea)
-		boxVertical.add(boxHorizontal)
-
-		textPanel.add(boxVertical)
-
-		self.tab.add(textPanel, BorderLayout.NORTH) 
-
-		tabbedPane = swing.JTabbedPane()
-		self.tab.add("Center", tabbedPane);
-
-		firstTab = swing.JPanel()
-		firstTab.layout = BorderLayout()
-		tabbedPane.addTab("Encode", firstTab)
-
-		secondTab = swing.JPanel()
-		secondTab.layout = BorderLayout()
-		tabbedPane.addTab("Decode", secondTab)
-
-		thirdTab = swing.JPanel()
-		thirdTab.layout = BorderLayout()
-		tabbedPane.addTab("Hash", thirdTab)
-
-		# Panel for the encoders. Each label and text field
-		# will go in horizontal boxes which will then go in 
-		# a vertical box
-		encPanel = swing.JPanel()
-		boxVertical = swing.Box.createVerticalBox()
-
-		# Add the vertical box to the Encode tab
-		firstTab.add(boxVertical, "Center")
